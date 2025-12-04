675b1016245dbb6a0a99684a958f3d29d75b1bab
Krzysztof Kopera <krzysztof.kopera@insc.pl>
2025-12-04 22:47:04 +0100

    poszukiwanie zera

--- README.md
+++ README.md
@@ -1,36 +1,12 @@
-# aoc2025
-
-FIXME: description
-
-## Installation
-
-Download from http://example.com/FIXME.
-
 ## Usage
 
-FIXME: explanation
-
-    $ java -jar aoc2025-0.1.0-standalone.jar [args]
-
-## Options
-
-FIXME: listing of options this app accepts.
-
-## Examples
-
-...
-
-### Bugs
-
-...
-
-### Any Other Sections
-### That You Think
-### Might be Useful
+```
+lein test :only aoc2025.core-test/day01
+```
 
 ## License
 
-Copyright © 2025 FIXME
+Copyright © 2025 Kuna i Lepak Akademia Informatyki
 
 This program and the accompanying materials are made available under the
 terms of the Eclipse Public License 2.0 which is available at
--- src/aoc2025/day01.clj
+++ src/aoc2025/day01.clj
@@ -7,21 +7,41 @@
 
 (defn rotate [position [dir step]]
   (case dir
-    "L" (mod (- position step) 100)
-    "R" (mod (+ position step) 100)))
-
-(rotate (rotate 50 ["L" 68]) ["L" 30])
+    "L" [(mod (- position step) 100) (quot (- position step) 100)]
+    "R" [(mod (+ position step) 100) (quot (- position step) 100)]))
 
 (defn part1
   [input]
-  (count (filter zero? (reductions rotate 50 (->> (string/split-lines input)
-                                                  (map #(re-matches #"(L|R{1})(\d+)" %))
-                                                  (map (fn [[_ dir step]] (vector dir (Long/parseLong step)))))))))
+  (count
+   (filter zero?
+           (reductions rotate 50 (->> (string/split-lines input)
+                                      (map #(re-matches #"(L|R{1})(\d+)" %))
+                                      (map (fn [[_ dir step]] (vector dir (Long/parseLong step)))))))))
 (defn part2
   [input]
   ;; tu logika dla części 2
   (apply + (map int input)))
 
+
+(comment
+  (rotate (rotate 50 ["L" 68]) ["L" 30])
+
+  (defn dial-path
+    [start dir steps]
+    (let [start (mod start 100)
+          sign  (case dir
+                  "R"  1
+                  "L" -1)]
+      ;; reductions robi: start -> (start+sign) -> (start+2*sign) -> ...
+      ;; My bierzemy TYLKO kolejne kroki, czyli drop 1.
+      (->> (reductions
+            (fn [pos _]
+              (mod (+ pos sign) 100))
+            start
+            (range steps))
+           (next))))   ;; usuwamy pozycję startową
+
+  (print (dial-path 50 "L" 68)))
 
 (-> (slurp (io/resource "day01/input"))
     part1)
